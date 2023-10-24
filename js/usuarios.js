00bdf0b9a1f26490f62a2582f29f6d98efbd31dc
Luiz Jacson <luiz.jacson@souunit.com.br>
2023-10-24 14:00:31 -0300

    Criação do menu superior e lateral e atribuindo responsividade; validação de login

--- js/usuarios.js
+++ js/usuarios.js
@@ -1,4 +1,32 @@
-var usuarios = {
-    "user": "luiz",
-    "password": "123"
-}
+// export function usuarios() {
+//     return [{
+//         "id": 1,
+//         "user": "aluno",
+//         "userName": "aluno da silva",
+//         "password": "123",
+//         "tipoUsuario": "aluno"
+//     },
+//     {
+//         "id": 2,
+//         "user": "professor",
+//         "userName": "professor da silva",
+//         "password": "123",
+//         "tipoUsuario": "professor"
+//     },
+//     {
+//         "id": 3,
+//         "user": "responsavel",
+//         "userName": "responsável da silva",
+//         "password": "123",
+//         "tipoUsuario": "responsavel"
+//     },
+//     {
+//         "id": 4,
+//         "user": "coordenador",
+//         "userName": "coordenador da silva",
+//         "password": "123",
+//         "tipoUsuario": "coordenador"
+//     },
+//     ]
+// }
+
--- js/validarLogin.js
+++ js/validarLogin.js
@@ -1,7 +1,34 @@
-var usuarios = [{
-    "user": "luiz",
-    "password": "123"
-}]
+const usuarios = [{
+        "id": 1,
+        "user": "aluno",
+        "userName": "aluno da silva",
+        "password": "123",
+        "tipoUsuario": "aluno"
+    },
+    {
+        "id": 2,
+        "user": "professor",
+        "userName": "professor da silva",
+        "password": "123",
+        "tipoUsuario": "professor"
+    },
+    {
+        "id": 3,
+        "user": "responsavel",
+        "userName": "responsável da silva",
+        "password": "123",
+        "tipoUsuario": "responsavel"
+    },
+    {
+        "id": 4,
+        "user": "coordenador",
+        "userName": "coordenador da silva",
+        "password": "123",
+        "tipoUsuario": "coordenador"
+    },
+    ]
+
+
 
 var user = document.getElementById("usuario")
 var senha = document.getElementById("senha")
@@ -9,6 +36,7 @@ var botaoLogin = document.getElementById("botaoLogin")
 var formulario = document.getElementById("form")
 
 botaoLogin.onclick = function(){
+    var invalidLabel = document.getElementById('usuarioInvalido')
     formulario.onsubmit = function(event){
         event.preventDefault();
     }
@@ -17,8 +45,16 @@ botaoLogin.onclick = function(){
         if(usuarios[i].user == user.value && usuarios[i].password == senha.value){
             logou = true
             window.location.href = "../professorView/index.html";
+            break;
         }else{
-            alert("nao logou")
+            console.log(user.value)
+            console.log(senha.value)
         }
+    }
+    if(logou == false){
+        console.log(invalidLabel)
+        console.log(logou)
+        invalidLabel.setAttribute('style', 'color: red;margin-bottom: 10px;')
+        invalidLabel.style.display = "block";
     }
 }
